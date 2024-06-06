5c1156d567474a6d877c19b1555f34d8293eb03f
Arshdeep Arora <arsharora017@gmail.com>
2024-06-06 11:49:43 -0400

    setup router and create sign in form

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 - create-react-app
 - Configured TailwindCSS
+- Header
+- Login Form
+- Sign up form2
+
+# Features
+
+- Login/Sign Up
+  - Sign In / Sign up Form
+  - Redirects to Browse Page
+- Browse (after authentication)
+
+  - Header
+  - Main Movie
+    - Trailer in background
+    - Title & Discription
+    - MovieSuggestions
+      - MovieLists \* N
+
+- NetflixGPT
+  - Search Bar
+  - Movie Suggestions
---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/f2e98033-57b5-4cf9-993d-ee774904fc5e/CA-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt=""
+        ></img>
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+        <p className="py-4">New to Netflix? Sign Up Now</p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
